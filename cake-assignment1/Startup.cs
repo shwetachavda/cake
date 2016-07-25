909e25ca09f5b5e61ca01f1f9e991b03e1b103dd
shwetachavda <shwetachavda13@gmail.com>
2016-07-24 23:12:10 -0400

    Add project files.

---
+++ cake-assignment1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cake_assignment1.Startup))]
+namespace cake_assignment1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
